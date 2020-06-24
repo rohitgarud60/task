ab809d14846e05f576f9d9c914e11614f51d44df
rohitgarud60 <52846332+rohitgarud60@users.noreply.github.com>
2020-06-24 18:34:20 +0530

    second commit

--- exercise.py
+++ exercise.py
@@ -6,6 +6,6 @@ while True:
   result = subprocess.run(cmd, stdout=subprocess.PIPE).stdout.decode('utf-8')
   datetime_object = datetime.datetime.now()
   with open('index.html', "w") as f:
-    f.write("f<html><body>Hi from python, time: <b>{datetime_object}</b></p>\n")
+    f.write("f<html><body>Hello from python, time: <b>{datetime_object}</b></p>\n")
     f.write("f<p>from C: <b>{result}</b></p></body></html>\n")
 time.sleep(5)
